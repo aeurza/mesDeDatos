03a1ef76d8c3981e0b86bfbdedc72b9f43e05159
aeurza <80500144+aeurza@users.noreply.github.com>
2023-03-08 13:12:05 -0400

    Este es inicio de los módulos del MICROSOFT CHALLENGE

---
+++ hello.py
@@ -0,0 +1,18 @@
+# importamos algunas librerias
+from datetime import date
+
+print(date.today())
+
+print("La fecha de hoy es: " + str(date.today()))
+
+# suma de 2 digitos
+
+sum = 1 + 2
+print(sum)
+
+
+# Saludamos al mundo (Hello World)
+
+print("hello World")
+
+
