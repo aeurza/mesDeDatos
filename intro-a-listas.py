fe3ea2ad6fecadc0293dd21b50e5009415a8c64e
arixo <ariel.urzagasteq@hotmail.com>
2023-03-11 00:56:15 -0400

    Se agreaga el programa  del uso de listas y algunos metodos

---
+++ intro-a-listas.py
@@ -0,0 +1,109 @@
+# Introducción
+# Completado
+
+# Como desarrollador, trabajará con frecuencia con conjuntos de datos. Es posible que tenga que administrar varios nombres, edades o direcciones. Almacenar cada valor en una variable individual hace que el código sea más difícil de leer y escribir. Para almacenar varios valores, puede usar una lista de Python.
+
+# Escenario: Trabajar en una aplicación de planetario
+# Imagine es un desarrollador que quiere crear una aplicación para trabajar con una lista de planetas. Quiere preguntar al usuario el nombre de un planeta y mostrar los planetas más cercanos al sol y los más alejados de él.
+
+# En este módulo, descubrirá cómo usar listas de Python y algunas de las operaciones más comunes.
+
+# ¿Qué descubriré?
+# Después de completar este módulo, podrá:
+
+# Identificar cuándo se debe usar una lista.
+# Crea una lista.
+# Acceder a un elemento determinado de una lista mediante índices.
+# Insertar elementos al final de una lista.
+# Ordenar y segmentar una lista.
+# ¿Cuál es el objetivo principal?
+# Al final de este módulo, comprenderá cuándo usar una estructura de lista y cómo puede ayudar a organizar los datos.
+
+
+# LAS LISTAS SE DEFINEN POR:
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# para recorrer la lista y siempre empieza por el 0
+
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+planets[3] = "Red Planet"
+print("Mars is also known as", planets[3])
+
+
+# METODO len ES LA CANTIDAD DE ITEMS QUE TIENE LA LISTA
+
+number_of_planets = len(planets)
+
+print("There are", number_of_planets, "planets in the solar system.")
+
+# OPERACIONES  DE VALORES DE LISTA
+# Las listas de Python son dinámicas: puede agregar y quitar elementos después de crearlas. Para agregar un elemento a una lista, use el método .append(value).
+
+# .append agrega un ITEM al final de la lista
+
+# Por ejemplo, el código siguiente agrega la cadena "Pluto" al final de la lista planets:
+
+planets.append("Pluto")
+print(planets)
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+print("""
+----->.append()  AÑADE UN ITEM AL FINAL DE LA LISTA<----
+""")
+print(planets)
+ 
+# MÉTODO .pop.... quita el ultimo item de la lista
+
+
+
+planets.pop()  # Goodbye, Pluto
+number_of_planets = len(planets)
+print("No, there are definitely", number_of_planets, "planets in the solar system.")
+
+print("""
+----->.pop() QUITA EL ULTIMO ITEM DE LA LISTA<----
+""")
+print(planets)
+
+
+# Uso de índices negativos
+# Ha visto cómo usar índices para capturar un elemento individual en una lista:
+
+print("The first planet is", planets[0])
+
+# Los índices comienzan en cero y van en aumento. Los índices negativos comienzan al final de la lista y van hacia atrás.
+
+# En el ejemplo siguiente, un índice de -1 devuelve el último elemento de una lista. Un índice de -2 devuelve el penúltimo.
+
+print("""
+cuando el indice es negativo [-2] la lista se recorra de atrás hacia a adelante""")
+
+print(planets)
+print("The last planet is", planets[-1])
+print("The penultimate planet is", planets[-2])
+
+
+###### Uso de min() y max() con listas ######
+
+# Python tiene funciones integradas para calcular los números más grandes y más pequeños de una lista. La función max() devuelve el número más grande y min() devuelve el más pequeño. Por lo tanto, min(gravity_on_planets) devuelve el número más pequeño de la lista gravity_on_planets, que es 0,377 (Marte).
+
+# El código siguiente calcula los pesos mínimo y máximo en el sistema solar mediante esas funciones:
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+
+
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "kg")
+
+gravity_on_planets = [5.378, 6.907, 1, 1.377, 2.36, 0.916, 9.889, 0.12]
+
+print(gravity_on_planets)
+print(sorted(gravity_on_planets))
+print(sorted(gravity_on_planets,reverse=True))
+
+
